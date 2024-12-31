20a1a919e85359cc429996c389ae1eb29ae40aef
Mahmut Enes <32mahmutenes32@gmail.com>
2024-12-31 22:38:24 +0300

    DNS Headers

--- app/main.go
+++ app/main.go
@@ -8,6 +8,26 @@ import (
 // Ensures gofmt doesn't remove the "net" import in stage 1 (feel free to remove this!)
 var _ = net.ListenUDP
 
+type DNSHeader struct {
+	ID      uint16
+	QR      bool
+	OPCODE  uint8
+	AA      bool
+	TC      bool
+	RD      bool
+	RA      bool
+	Z       uint8
+	RCODE   uint8
+	QDCOUNT uint16
+	ANCOUNT uint16
+	NSCOUNT uint16
+	ARCOUNT uint16
+}
+
+type DNSMessage struct {
+	Header DNSHeader
+}
+
 func main() {
 	fmt.Println("Logs from your program will appear here!")
 
@@ -36,12 +56,58 @@ func main() {
 		receivedData := string(buf[:size])
 		fmt.Printf("Received %d bytes from %s: %s\n", size, source, receivedData)
 
-		// Create an empty response
-		response := []byte{}
+		header := DNSHeader{
+			ID:      1234,
+			QR:      true,
+			OPCODE:  0,
+			AA:      false,
+			TC:      false,
+			RD:      false,
+			RA:      false,
+			Z:       0,
+			RCODE:   0,
+			QDCOUNT: 0,
+			ANCOUNT: 0,
+			NSCOUNT: 0,
+			ARCOUNT: 0,
+		}
 
+		// Create an empty response
+		response := encodeDNSHeader(header)
+		fmt.Println("Response:", response)
 		_, err = udpConn.WriteToUDP(response, source)
 		if err != nil {
 			fmt.Println("Failed to send response:", err)
 		}
 	}
+}
+
+func boolToByte(value bool) byte {
+	if value {
+		return 1
+	}
+	return 0
+}
+
+func encodeDNSHeader(header DNSHeader) []byte {
+	buffer := make([]byte, 12)
+
+	// Integers should be encoded in big-endian
+	buffer[0] = byte(header.ID >> 8)
+	buffer[1] = byte(header.ID)
+
+	// Encode the flags
+	flags := uint16(0)
+	flags |= uint16(boolToByte(header.QR)) << 15
+	flags |= uint16(header.OPCODE) << 11
+	flags |= uint16(boolToByte(header.AA)) << 10
+	flags |= uint16(boolToByte(header.TC)) << 9
+	flags |= uint16(boolToByte(header.RD)) << 8
+	flags |= uint16(boolToByte(header.RA)) << 7
+	flags |= uint16(header.Z) << 4
+
+	buffer[2] = byte(flags >> 8)
+	buffer[3] = byte(flags)
+
+	return buffer
 }
